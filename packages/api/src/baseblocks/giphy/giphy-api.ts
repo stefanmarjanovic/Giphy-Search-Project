6d4a582af0f6425937daaa30b53d5045efae8776
stefanmarjanovic <admin@stefanmarjanovic.com>
2025-08-24 16:59:16 +1000

    add assertion q is a string and only ever a string

--- packages/api/src/baseblocks/giphy/giphy-api.ts
+++ packages/api/src/baseblocks/giphy/giphy-api.ts
@@ -4,17 +4,18 @@ import { searchGiphy } from './giphy.service';
 const router = express.Router();
 
 // Express route to handle Giphy search requests
-router.get('/search-giphy', asyncHandler( async (req, res) => {
-  const { q } = req.query;
-  console.log('Trying Giphy API request');   
-  try {const results = await searchGiphy(String(q), 12);
+router.get('/search-giphy', asyncHandler(async (req, res) => {
+  const q = req.query.q;
+  const queryString = typeof q === 'string' ? q : '';
+  console.log('Trying Giphy API request');
+  try {
+    const results = await searchGiphy(queryString, 12);
     res.json(results);
   } catch (error) {
-      console.error('Giphy API error:', error);
-      res.status(500).json({ error: 'Failed to fetch data from Giphy API', details: error?.message || error });
-}
-  
-  console.log('Completed Giphy API request'); 
+    console.error('Giphy API error:', error);
+    res.status(500).json({ error: 'Failed to fetch data from Giphy API', details: error?.message || error });
+  }
+  console.log('Completed Giphy API request');
 }));
 
 // Express Helper to handle async errors in route handler
