e4251cc171e586c2bfa6506e2c9bfc247f05412d
stefanmarjanovic <admin@stefanmarjanovic.com>
2025-08-24 17:17:26 +1000

    remove unsafe assignment of any value
    explicitly assign resutls as type Array of Giphy interface type

--- packages/api/src/baseblocks/giphy/giphy-api.ts
+++ packages/api/src/baseblocks/giphy/giphy-api.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { searchGiphy } from './giphy.service';
+import { Giphy } from '@baseline/types/giphy';
 
 const router = express.Router();
 
@@ -9,8 +10,8 @@ router.get('/search-giphy', asyncHandler(async (req, res) => {
   const queryString = typeof q === 'string' ? q : '';
   console.log('Trying Giphy API request');
   try {
-    const results = await searchGiphy(queryString, 12);
-    res.json(results);
+  const results: Array<Giphy> = await searchGiphy(queryString, 12);
+  res.json(results);
   } catch (error) {
     console.error('Giphy API error:', error);
     res.status(500).json({ error: 'Failed to fetch data from Giphy API', details: error?.message || error });
